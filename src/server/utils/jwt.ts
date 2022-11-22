73cd08c2e1914ba9e1d85ce4d424541ae76f1ab2
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-11-22 10:00:15 -0300

    feat(server/jwt): create verifyJwt function

--- src/server/utils/jwt.ts
+++ src/server/utils/jwt.ts
@@ -12,3 +12,18 @@ export const signJwt = (
         algorithm: 'RS256',
     });
 };
+
+export const verifyJwt = <T>(
+    token: string,
+    key: 'accessTokenPublicKey' | 'refreshTokenPublicKey'
+): T | null => {
+    try {
+        const publicKey = Buffer.from(customConfig[key], 'base64').toString(
+            'ascii'
+        );
+        return jwt.verify(token, publicKey) as T;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
