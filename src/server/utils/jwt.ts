a2e489160c8a02aeb4e602242aac1b22de499a33
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-11-22 09:46:57 -0300

    feat(server): create jwt file with sign function

---
+++ src/server/utils/jwt.ts
@@ -0,0 +1,14 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
+import customConfig from '../config/default';
+
+export const signJwt = (
+    payload: Object,
+    key: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey',
+    options: SignOptions = {},
+) => {
+    const privateKey = Buffer.from(customConfig[key], 'base64').toString('ascii');
+    return jwt.sign(payload, privateKey, {
+        ...(options && options),
+        algorithm: 'RS256',
+    });
+};
